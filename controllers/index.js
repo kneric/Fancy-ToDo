1df9c9d41223f9226fab9388d549f34e6f78f07a
kneric <brian.pradipta.a@gmail.com>
2018-08-13 06:44:01 +0700

    added signup

--- controllers/index.js
+++ controllers/index.js
@@ -21,9 +21,43 @@ const signin = (req, res) {
 }
 
 const signup = (req, res){
+  const {username, email, password} = req.body;
+
+  User.findOne({username})
+  .then(user => {
+    if(!user){
+      User.create({
+        username,
+        email,
+        password
+      })
+      .then(createdUser=> {
+        let token = jwt.sign({_id: createdUser._id}, process.env.secretKey)
+        res.status(201).json({
+          token: token, 
+          message: 'user created'
+        })
+      })
+      .catch(err=> {
+        res.status(400).json({message: err})
+      })
+    } else {
+      res.status(400).json({message: 'username already taken'})
+    }
+  })
+  .catch(err=> {
+    res.status(500).json({message: err})
+  })
+}
+
+const loginFB = (req, res){
+  let authResponse = req.body;
+  let url = `https://graph.facebook.com/me?fields=id,name,email&access_token=${authResponse.accessToken}`;
   
 }
 
 module.exports = {
-  
+  signin,
+  signup,
+  loginFB
 };
