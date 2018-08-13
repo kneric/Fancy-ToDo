3a91983d85157255158df204b80039680f368b0a
kneric <brian.pradipta.a@gmail.com>
2018-08-13 08:16:32 +0700

    added update todo

--- controllers/todo.js
+++ controllers/todo.js
@@ -4,10 +4,56 @@ const loggedInUser = jwt.verify(req.body.token, process.env.secretKey)
 
 const createTodo = (req, res)=> {
   const {task, dueDate, description, priority} = req.body
-  
 
   Todo.create({
+    userId: loggedInUser._id,
+    task,
+    dueDate,
+    description,
+    priority
+  })
+  .then(createdTask => {
+    res.status(201).json({
+      createdTask, 
+      message:'task created'
+    })
+    .catch(err => {
+      res.status(500).json({message: err});
+    })
+  })
+}
 
+const listTodo = (req, res) => {
+  Todo.find({
+    userId: loggedInUser._id
+  })
+  .then(todos => {
+    if(todos){
+      res.status(200).json({todos, message: `here's your todos`})
+    } else {
+      res.status(200).json({message: `you haven't created any todo`})
+    }
+  })
+  .catch(err =>{
+    res.status(500).json({message: err});
+  })
+}
+
+const updateTodo = (req, res) => {
+  Todo.findById(req.params.id)
+  .then(todo => {
+    return todo.update({
+      $set : req.body
+    })
+    .then (updatedtodo=> {
+      res.status(200).json({
+        updatedtodo, 
+        message:'todo updated'
+      })
+    })
+  })
+  .catch(err=> {
+    res.status(500).json({message: err});
   })
 }
 
